b5f4e74b99be2e8ec7a43f1c8c246db69246cc1e
natagetup <nata.getup@gmail.com>
2022-12-07 20:01:35 +0100

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface ButtonProps {
+    label: string
+}
+
+export default function Button (props: ButtonProps) {
+  return <button>{props.label}</button>  
+}
+
